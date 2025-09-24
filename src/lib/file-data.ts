2658991c425f712a4428bc997e61499b9dc0f66f
Ramy Ramadan <roumio777@gmail.com>
2025-09-24 14:06:13 +0000

    هذه نفس ايقونة pediatrics ستخدم غيرها

--- src/lib/file-data.ts
+++ src/lib/file-data.ts
@@ -48,7 +48,7 @@ export const allSubjectIcons: { [key: string]: LucideIcon } = {
     'Leadership Skills': Users,
     'Biomedical Research': TestTube2,
     'Pediatrics 1': Baby,
-    'Gynecology & Obstetrics 1': Baby,
+    'Gynecology & Obstetrics 1': Dna,
     Ophthalmology: Eye,
     Otorhinolaryngology: Ear,
     'Community Medicine': Users,
@@ -58,7 +58,7 @@ export const allSubjectIcons: { [key: string]: LucideIcon } = {
     'Hospital Management & Health Economics': FolderKanban,
     'UE 2': Star,
     'Pediatrics 2': Baby,
-    'Gynecology & Obstetrics 2': Baby,
+    'Gynecology & Obstetrics 2': Dna,
     'Emergency Medicine': Ambulance,
     'Family Medicine': Home,
     'Clinical Toxicology & Forensic Medicine': Scale,
@@ -192,6 +192,7 @@ levelsRaw.forEach(level => {
         }
     });
 });
+
 
 
 
