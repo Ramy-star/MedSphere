e32b18a05118173fb93c57d9cfb0e7e2a1fd7d9b
Ramy Ramadan <roumio777@gmail.com>
2025-09-25 19:28:07 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/contentService.ts
+++ src/lib/contentService.ts
@@ -1,7 +1,7 @@
 
 'use client';
 import { db } from '@/firebase';
-import { collection, writeBatch, query, where, getDocs, orderBy, doc, setDoc, getDoc, updateDoc, deleteDoc, runTransaction } from 'firebase/firestore';
+import { collection, writeBatch, query, where, getDocs, orderBy, doc, setDoc, getDoc, updateDoc, runTransaction } from 'firebase/firestore';
 import { allContent as seedData } from './file-data';
 import { v4 as uuidv4 } from 'uuid';
 import { naturalSort } from './sort';
@@ -269,3 +269,5 @@ export const contentService = {
     }
   }
 };
+
+    
