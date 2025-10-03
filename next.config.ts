e6aa954fa619d70e4e573e0877a8087b2fb2b6e6
Ramy Ramadan <roumio777@gmail.com>
2025-10-03 20:56:40 +0000

    ChunkLoadError     at __webpack_require__.f.j (https://6000-firebase-med

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development', // This explicitly disables PWA in development
   runtimeCaching: [
     {
       urlPattern: /^https:\/\/res\.cloudinary\.com\/.*/i,
