3016b53d092f0fedae211e66b82d26bd8b923c2d
Ramy Ramadan <roumio777@gmail.com>
2025-10-09 09:11:52 +0000

    المربع الابيض في زر الايقاف اجعله في منتصف الدائرة الزرقاء !!

--- src/components/SendStopButton.tsx
+++ src/components/SendStopButton.tsx
@@ -68,7 +68,7 @@ export default function SendStopButton({ onSend, onStop, isSending, disabled, si
           >
             <div className={`flex items-center justify-center ${btn}`}>
               <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect x="2" y="2" width="20" height="20" rx="2" fill="#ffffff" />
+                <rect x="4" y="4" width="16" height="16" rx="2" fill="#ffffff" />
               </svg>
             </div>
           </button>
