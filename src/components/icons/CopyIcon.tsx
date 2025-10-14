7d270e9de4ddd4e642217fde474007ea8f63e744
Ramy Ramadan <roumio777@gmail.com>
2025-10-14 17:20:59 +0000

    لم يتم تنفيذ ما طلبت !!!
    ايقونة نسخ في الكود الاصلي يعني خذها من الكود

--- src/components/icons/CopyIcon.tsx
+++ src/components/icons/CopyIcon.tsx
@@ -1,4 +1,3 @@
-
 import { cn } from "@/lib/utils";
 import type { LucideProps } from "lucide-react";
 import React from "react";
@@ -19,19 +18,17 @@ export const CopyIcon = React.forwardRef<SVGSVGElement, LucideProps>(
       ref={ref}
       {...props}
     >
-        <g clipPath="url(#clip0_4418_9596)">
-            <path d="M16 12.9V17.1C16 20.6 14.6 22 11.1 22H6.9C3.4 22 2 20.6 2 17.1V12.9C2 9.4 3.4 8 6.9 8H11.1C14.6 8 16 9.4 16 12.9Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M22 6.9V11.1C22 14.6 20.6 16 17.1 16H16V12.9C16 9.4 14.6 8 11.1 8H8V6.9C8 3.4 9.4 2 12.9 2H17.1C20.6 2 22 3.4 22 6.9Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-        </g>
-        <defs>
-            <clipPath id="clip0_4418_9596">
-            <rect width="24" height="24" fill="white"/>
-            </clipPath>
-        </defs>
+      <g clipPath="url(#clip0_4418_9596)">
+        <path d="M16 12.9V17.1C16 20.6 14.6 22 11.1 22H6.9C3.4 22 2 20.6 2 17.1V12.9C2 9.4 3.4 8 6.9 8H11.1C14.6 8 16 9.4 16 12.9Z" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M22 6.9V11.1C22 14.6 20.6 16 17.1 16H16V12.9C16 9.4 14.6 8 11.1 8H8V6.9C8 3.4 9.4 2 12.9 2H17.1C20.6 2 22 3.4 22 6.9Z" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+      </g>
+      <defs>
+        <clipPath id="clip0_4418_9596">
+          <rect width="24" height="24" fill="white"/>
+        </clipPath>
+      </defs>
     </svg>
   )
 );
 
 CopyIcon.displayName = 'CopyIcon';
-
-    
