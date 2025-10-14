fd8eed6911c0e7e0b16994add3d316394fb232fa
Ramy Ramadan <roumio777@gmail.com>
2025-10-14 15:29:20 +0000

    في صفحة التي تظهر اول مرة فتح الموقع في هذه النافذة لوجو الموقع يبدو مقص

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 
 export const Logo = ({ className }: { className?: string }) => (
-    <svg width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg" className={cn("h-10 w-auto", className)}>
+    <svg width="200" height="200" viewBox="0 0 225 200" fill="none" xmlns="http://www.w3.org/2000/svg" className={cn("h-10 w-auto", className)}>
         <mask id="mask0_209_54" style={{maskType: 'alpha'}} maskUnits="userSpaceOnUse" x="0" y="0" width="200" height="200">
             <path opacity="0.7" d="M100 200C155.228 200 200 155.228 200 100C200 44.7715 155.228 0 100 0C44.7715 0 0 44.7715 0 100C0 155.228 44.7715 200 100 200Z" fill="#1D4ED8"/>
         </mask>
