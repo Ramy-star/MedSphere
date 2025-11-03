3fa9c10f4a410c0b02b0598aacf27af5397beadc
Ramy Ramadan <roumio777@gmail.com>
2025-11-03 22:57:54 +0000

    Try fixing this error: `Runtime Error: Error: Element type is invalid: e

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -23,7 +23,6 @@ const PopoverContent = React.forwardRef<
         "z-[9999] w-72 rounded-2xl bg-slate-900/70 p-4 text-popover-foreground shadow-md backdrop-blur-lg outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
         className
       )}
-      onWheel={(e) => e.stopPropagation()}
       {...props}
     />
   </PopoverPrimitive.Portal>
