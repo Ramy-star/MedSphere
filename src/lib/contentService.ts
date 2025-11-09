d577d8243f5fa5e48bb1777f01db5bd700edb6c4
Ramy Ramadan <roumio777@gmail.com>
2025-11-09 09:08:36 +0000

    ما المشكلة 10:56:55.714 Running build in Washington, D.C., USA (East) –

--- src/lib/contentService.ts
+++ src/lib/contentService.ts
@@ -8,7 +8,7 @@ import { errorEmitter } from '@/firebase/error-emitter';
 import { FirestorePermissionError } from '@/firebase/errors';
 import { nanoid } from 'nanoid';
 import type { PDFDocumentProxy } from 'pdfjs-dist';
-import { offlineStorage } from '@/lib/offline';
+import { offlineStorage } from './offline';
 import type { Lecture } from './types';
 import type { UserProfile } from '@/stores/auth-store';
 import * as pdfjs from 'pdfjs-dist';
