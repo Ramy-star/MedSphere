a5e1ce4ba13ef1916414afa4bebbd8e46e69ebed
Ramy Ramadan <roumio777@gmail.com>
2026-01-10 13:50:06 +0000

    Try fixing this error: `Console Error: React has detected a change in th

--- app/src/app/(main)/layout.tsx
+++ app/src/app/(main)/layout.tsx
@@ -161,7 +161,7 @@ export default function MainLayout({
           </div>
         </motion.main>
       </div>
-      <FloatingAssistant user={user} />
+      <FloatingAssistant />
        <Suspense fallback={null}>
         {previewItem && (
           <FilePreviewModal
